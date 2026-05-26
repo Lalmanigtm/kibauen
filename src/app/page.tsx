0ff6b096353a5d13585967cbc02b996e6e96188e
Saurab Gautam <missionprojects250@gmail.com>
2026-05-26 13:06:22 +0545

    01:setup a prisma with neondb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
-import { Card } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-// import { Chart } from "@/components/ui/chart";
+// import { Card } from "@/components/ui/card";
+// import { Button } from "@/components/ui/button";
+// import { Calendar } from "@/components/ui/calendar";
+// // import { Chart } from "@/components/ui/chart";
 
-const page = () => {
-  return (
-    <div className="text-red-600 font-extrabold text-4xl">
-      this is a page section
-      <Card />
-      <Button title="rammu" variant={"secondary"} />
-      <Calendar />
-      {/* <Chart /> */}
-    </div>
-  );
+// const page = () => {
+//   return (
+//     <div className="text-red-600 font-extrabold text-4xl">
+//       this is a page section
+//       <Card />
+//       <Button title="rammu" variant={"secondary"} />
+//       <Calendar />
+
+//     </div>
+//   );
+// };
+
+// export default page;
+import { prisma } from "@/lib/db";
+
+const page = async () => {
+  const users = await prisma.user.findMany();
+  return <div>{JSON.stringify(users, null, 2)}</div>;
 };
 
 export default page;
